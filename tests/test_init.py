5f9f4fb9995087883da946976a6747b3fe6fb439
Shelley Gao <syugao@gmail.com>
2023-07-12 14:26:44 -0700

    Adding tarfile download test and testing environment for runner

--- tests/test_init.py
+++ tests/test_init.py
@@ -1,18 +1,28 @@
 import pytest
 import os
+import tarfile
 from pathlib import Path
-from dotenv import load_dotenv
+# from dotenv import load_dotenv
 from requests import JSONDecodeError
 
 from paths import TEST_FILES_PATH
 
+# env = load_dotenv(TEST_FILES_PATH / 'secrets.env')
+
 TEST_PATH = TEST_FILES_PATH / "new_repo"
-env = load_dotenv(TEST_FILES_PATH / 'testing.env')
 
 cache_path = TEST_FILES_PATH / '.cache'
 cache_path.mkdir(exist_ok=True)
 
 from virtool_cli import init
+
+def test_fetch_callers_tarfile():
+    caller_path = init.fetch_callers(
+        api_url=os.environ['CALLER_API_URL'],
+        cache_path=cache_path,
+        gh_token=os.environ['GH_PAT'])
+    
+    assert tarfile.is_tarfile(caller_path)
 
 def test_bad_extant_url():
     with pytest.raises(KeyError):
@@ -20,5 +30,7 @@ def test_bad_extant_url():
             api_url='https://api.github.com',
             cache_path=cache_path)
 
-if __name__ == '__main__':
-    print(os.getenv("CALLER_API_URL"))
+# if __name__ == '__main__':
+#     # env = load_dotenv(TEST_FILES_PATH / 'secrets.env')
+
+#     test_fetch_callers_tarfile()
