b33102b014d64c4cb212a16221b4bd66681c731f
Shelley Gao <syugao@gmail.com>
2023-07-18 16:12:22 -0700

    legacy_utils.get_otu_paths() -> utils.traverse.get_all_otu_paths()

--- virtool_cli/utils/traverse.py
+++ virtool_cli/utils/traverse.py
@@ -1,6 +1,21 @@
 import json
 from pathlib import Path
 
+def get_all_otu_paths(src_path: Path) -> list:
+    """
+    Generates a list of paths to all OTUs in a src directory.
+
+    :param src_path: Path to a src database directory
+    :return: List of paths to all OTU in a src directory
+    """
+    paths = []
+
+    for alpha in src_path.glob('[a-z]'):
+        otu_paths = [otu for otu in alpha.iterdir() if otu.is_dir()]
+        paths += otu_paths
+
+    return paths
+
 def get_otu_data(otu_path: Path) -> dict:
     """
     Returns data from an OTU's otu.json
