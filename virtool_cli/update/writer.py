52008bd8ef32ffffcb3a819bda16437b00bb0541
Shelley Gao <syugao@gmail.com>
2024-01-24 12:05:42 -0800

    Experimenting with AddWriter

--- virtool_cli/update/writer.py
+++ virtool_cli/update/writer.py
@@ -8,9 +8,180 @@ from virtool_cli.utils.reference import (
     search_otu_by_id,
     get_unique_ids,
 )
-from virtool_cli.utils.storage import write_records
+from virtool_cli.utils.storage import write_records, label_isolates
+from virtool_cli.utils.format import format_isolate
+from virtool_cli.utils.id_generator import generate_unique_ids
 
 DEFAULT_INTERVAL = 0.001
+
+
+class UpdateWriter:
+    def __init__(
+        self, src_path: Path, isolate_ids: set | None = None, sequence_ids = set | None
+    ):
+        self.src_path = src_path
+        self.isolate_ids = set() if isolate_ids is None else isolate_ids
+        self.sequence_ids = set() if sequence_ids is None else sequence_ids
+        self.logger = structlog.get_logger().bind(src_path=str(src_path))
+
+    async def run_loop(self, queue: asyncio.Queue):
+        self.logger.debug("Starting writer...")
+
+        while True:
+            packet = await queue.get()
+            otu_id, sequence_data = await process_packet(packet)
+            sequence_data = packet["data"]
+
+            logger = self.logger.bind(otu_id=otu_id)
+
+            otu_path = await get_otu_path(otu_id, self.src_path, logger)
+            if not otu_path:
+                queue.task_done()
+                continue
+
+            logger = logger.bind(otu_path=str(otu_path))
+
+            logger.debug("Writing packet...")
+            await write_records(
+                otu_path=otu_path,
+                new_sequences=sequence_data,
+                logger=logger
+            )
+
+            await asyncio.sleep(DEFAULT_INTERVAL)
+            queue.task_done()
+
+    async def write_otu_records(
+        self,
+        otu_path: Path,
+        new_sequences: list,
+        logger: structlog.BoundLogger = structlog.get_logger(),
+    ) -> list:
+        """
+        :param otu_path: A path to an OTU directory under a src reference directory
+        :param new_sequences: List of new sequences under the OTU
+        :param logger: Optional entry point for a shared BoundLogger
+        """
+        ref_isolates = await label_isolates(otu_path)
+
+        unassigned_sequence_ids = generate_unique_ids(
+            n=len(new_sequences), excluded=list(self.sequence_ids)
+        )
+
+        logger.debug(f"Writing {len(new_sequences)} sequences...")
+
+        new_sequence_paths = []
+        for seq_data in new_sequences:
+            # Assign an isolate path
+            isolate_data = seq_data.pop("isolate")
+            isolate_name = isolate_data["source_name"]
+            isolate_type = isolate_data["source_type"]
+
+            iso_id = self.assign_isolate_id(isolate_name, ref_isolates, self.isolate_ids, logger)
+
+            if not (otu_path / iso_id).exists():
+                new_isolate = await self.init_isolate(
+                    otu_path, iso_id, isolate_name, isolate_type, logger
+                )
+                ref_isolates[isolate_name] = new_isolate
+
+                self.isolate_ids.add(iso_id)
+
+                logger.info("Created a new isolate directory", path=str(otu_path / iso_id))
+
+            iso_path = otu_path / iso_id
+
+            # Assign a sequence ID and store sequence
+            sequence_id = unassigned_sequence_ids.pop()
+            logger.debug(
+                "Assigning new sequence",
+                seq_hash=sequence_id,
+            )
+
+            try:
+                await self.store_sequence(seq_data, sequence_id, iso_path)
+
+            except Exception as e:
+                logger.exception(e)
+
+            self.sequence_ids.add(sequence_id)
+            sequence_path = iso_path / f"{sequence_id}.json"
+
+            logger.info(
+                f"Wrote new sequence '{sequence_id}'", path=str(sequence_path)
+            )
+
+            new_sequence_paths.append(sequence_path)
+
+        return new_sequence_paths
+
+    def assign_isolate_id(self, isolate_name, ref_isolates, logger) -> str:
+        if isolate_name in ref_isolates:
+            iso_id = ref_isolates[isolate_name]["id"]
+            logger.debug(
+                "Existing isolate name found", iso_name=isolate_name, iso_hash=iso_id
+            )
+            return iso_id
+
+        else:
+            try:
+                iso_id = generate_unique_ids(n=1, excluded=list(self.isolate_ids)).pop()
+                return iso_id
+            except Exception as e:
+                logger.exception(e)
+                return ""
+    async def init_isolate(
+            self, otu_path: Path, isolate_id: str, isolate_name: str, isolate_type: str, logger
+    ) -> dict | None:
+        """
+        """
+        new_isolate = self.format_isolate(isolate_name, isolate_type, isolate_id)
+
+        await self.store_isolate(new_isolate, isolate_id, otu_path)
+
+        isolate_path = otu_path / f"{isolate_id}/isolate.json"
+        if isolate_path.exists():
+            logger.info("Created a new isolate directory", path=str(otu_path / isolate_id))
+            return new_isolate
+
+        else:
+            logger.error("Could not initiate isolate")
+            return None
+
+    @staticmethod
+    async def store_isolate(
+        isolate: dict, isolate_id: str, otu_path: Path
+    ):
+        """
+        Creates a new isolate directory and metadata file under an OTU directory,
+        then returns the metadata in dict form
+
+        :param isolate: Dictionary containing isolate metadata
+        :param isolate_id: Unique ID number for this new isolate
+        :param otu_path: Path to the parent OTU
+        :return: The unique isolate id
+        """
+        iso_path = otu_path / isolate_id
+        iso_path.mkdir()
+
+        with open(iso_path / "isolate.json", "w") as f:
+            json.dump(isolate, f, indent=4)
+
+    @staticmethod
+    async def store_sequence(sequence: dict, sequence_id: str, iso_path: Path):
+        """
+        Write sequence to isolate directory within the src directory
+
+        :param sequence: Dictionary containing formatted sequence data
+        :param sequence_id: Unique ID number for this new sequence
+        :param iso_path: Path to the parent isolate
+        :return: The unique sequence id (aka seq_hash)
+        """
+        sequence["_id"] = sequence_id
+        seq_path = iso_path / f"{sequence_id}.json"
+
+        with open(seq_path, "w") as f:
+            json.dump(sequence, f, indent=4, sort_keys=True)
 
 
 async def writer_loop(
